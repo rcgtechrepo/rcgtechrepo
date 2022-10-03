181eba9f422c263af18e16916b00bb1a847f1018
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-03 12:01:28 -0500

    test`

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   #name     = random_pet.rg_name.id
   #name	    = azurerm_resource_group.rg.id
-  name     = "fedex_rf_00001"
+  name     = "fedex_rg_00001"
   
 }
 
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "myVnet"
+  name                = "fedex_vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "mySubnet"
+  name                 = "fedex_subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -55,7 +55,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "myNIC"
+  name                = "fedex_nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
