59bc297a7ee2aea4eafc0ee04cc63730c93b07fe
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 11:41:51 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_sql_server" "example" {
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+    features {}
 
   tags = {
     environment = "production"
