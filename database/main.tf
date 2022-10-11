2ad237640f22ae65322f00c92cc768495f9cea32
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 12:16:47 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplesa"
+  name                     = "examplestorageaccount"
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   account_tier             = "Standard"
