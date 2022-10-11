6935d41358c159d14ce4829a59211442f69432a9
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 14:34:10 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -42,6 +42,23 @@ location            = var.resource_group_location
   }
 }
 
+module "dbseed" {
+  source             = "../../"
+  location           = "${azurerm_resource_group.g0.location}"
+  resource_group     = "${azurerm_resource_group.g0.name}"
+  db_server_fqdn     = "${azurerm_sql_server.server0.fully_qualified_domain_name}"
+  sql_admin_username = "${azurerm_sql_server.server0.administrator_login}"
+  sql_admin_password = "${azurerm_sql_server.server0.administrator_login_password}"
+  db_name            = "db_sample"
+  init_script_file   = "db.init.sql"
+  log_file           = "db.init.log"
+
+  tags = {
+    environment = "qa3"
+    project     = "acme/x"
+    provisioner = "terraform"
+  }
+}
 
 resource "azurerm_sql_firewall_rule" "example" {
   name                = "FirewallRule1"
