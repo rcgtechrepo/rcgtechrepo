978d89b00ea18f27267bfba3822d348af71499b0
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 14:59:24 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_sql_server" "example" {
 
 
 resource "azurerm_sql_database" "example" {
-  name                = var.ENV_NAME
+  name                = "RG_7380_DATABASE"
 location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_sql_server.example.name
