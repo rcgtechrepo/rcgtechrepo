78969059ab0f0a075277c6ca196a26252610b4e1
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 11:46:10 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -1,38 +1,45 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_sql_server" "example" {
-  name                         = "myexamplesqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-    features {}
-
-  tags = {
-    environment = "production"
-  }
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  #name     = random_pet.rg_name.id
+  #name	    = azurerm_resource_group.rg.id
+  #name     = "fedex_rg_00001"
+  name      = var.ENV_NAME
 }
+
+
+
+
+
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_sql_database" "example" {
-  name                = "myexamplesqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-
+resource "azurerm_sql_server" "example" {
+  name                         = "rcgmssqlserver"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  version                      = "12.0"
+  administrator_login          = "eric.perler@rcggs.com"
+  administrator_login_password = "Cdi6954c"
 
   tags = {
     environment = "production"
   }
 }
+
+resource "azurerm_sql_database" "example" {
+  name                = "myexamplesqldatabase"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.example.name
+
+  }
