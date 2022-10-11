41379d4eaf6604e3558276588e44479a03eed3ce
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 12:38:25 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -49,7 +49,13 @@ location            = var.resource_group_location
 }
 
 
-
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "FirewallRule1"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "999.999.999.999"
+}
 
 
 
