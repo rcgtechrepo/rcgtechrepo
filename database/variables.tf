f983ac909e543819b6e11876b207e40a8c62182d
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 11:51:34 -0500

    test

---
+++ database/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "ENV_NAME" {
+    type        = string
+    description = "This is an example input variable using env variables."
+}
+
