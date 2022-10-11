c158a51109134dfdca40ee74abf1f2f5052e2f3b
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 14:00:25 -0500

    test

--- database/outputs.tf
+++ database/outputs.tf
@@ -10,3 +10,8 @@ output "tls_private_key" {
   value     = tls_private_key.example_ssh.private_key_pem
   sensitive = true
 }
+
+output "connection_string" {
+  description = "Connection string for the Azure SQL Database created."
+  value       = "Server=tcp:${var.db_server_fqdn},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
