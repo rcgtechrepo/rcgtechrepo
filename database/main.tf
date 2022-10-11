4b907b0cf32ca1f710be118837b785cba3e99747
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-11 14:47:29 -0500

    test

--- database/main.tf
+++ database/main.tf
@@ -1,25 +1,178 @@
-# provider "azurerm" {
-#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
-#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
-#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
-#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
-# }
-
-locals {
-  db_server = "${split(".", var.db_server_fqdn)}"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_sql_database" "db" {
-  name                             = "${var.db_name}"
-  location                         = "${var.location}"
-  resource_group_name              = "${var.resource_group}"
-  server_name                      = "${local.db_server[0]}"
-  edition                          = "${var.db_edition}"
-  collation                        = "${var.collation}"
-  requested_service_objective_name = "${var.service_objective_name}"
-  create_mode                      = "Default"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  #name     = random_pet.rg_name.id
+  #name	    = azurerm_resource_group.rg.id
+  #name     = "fedex_rg_00001"
+  name      = var.ENV_NAME
+}
 
-  provisioner "local-exec" {
-    command = "sqlcmd -U ${var.sql_admin_username} -P ${var.sql_admin_password} -S ${var.db_server_fqdn} -d ${var.db_name} -i ${var.init_script_file} -o ${var.log_file}"
+
+
+
+
+
+
+resource "azurerm_sql_server" "example" {
+  name                         = "rcgmssqlserver"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  version                      = "12.0"
+  administrator_login          = "eric.perler@rcggs.com"
+  administrator_login_password = "Cdi6954c"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+
+
+resource "azurerm_sql_database" "example" {
+  name                = "myexamplesqldatabase"
+location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.example.name
+
+ tags = {
+    environment = "production"
+  }
+}
+
+
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "FirewallRule1"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "179.192.138.134"
+}
+
+
+
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "fedex_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "fedex_subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "fedex_PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "fedex_NetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "fedex_nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "fedex_vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "fedex_disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
 }
--- database/outputs.tf
+++ database/outputs.tf
@@ -1,19 +1,12 @@
-output "database_name" {
-  description = "Database name of the Azure SQL Database created."
-  value       = "${azurerm_sql_database.db.name}"
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "sql_server_fqdn" {
-  description = "Fully Qualified Domain Name (FQDN) of the Azure SQL server."
-  value       = "${var.db_server_fqdn}"
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
 
-output "connection_string" {
-  description = "Connection string for the Azure SQL Database created."
-  value       = "Server=tcp:${var.db_server_fqdn},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-}
-
-output "log" {
-  description = "Seed log"
-  value       = "${file("${var.log_file}")}"
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
 }
