6c06b46555813f385c847674eacb878534e38d4e
eperler_lt\eperler <eric.perler@rcggs.com>
2022-10-26 13:45:10 -0500

    test

--- datascience/main.tf
+++ datascience/main.tf
@@ -26,13 +26,7 @@ resource "azurerm_powerbi_embedded" "example" {
 
 
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplesa"
-  location            = var.resource_group_location
-  resource_group_name = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+
 
 resource "azurerm_sql_server" "example" {
   name                         = "rcgmssqlserver"
