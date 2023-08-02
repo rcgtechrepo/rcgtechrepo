0a9ddb13ee3fcc7b64d1d23b9668d4a1121ea5a7
unknown <eric.perler@rcggs.com>
2023-08-01 21:54:02 -0500

    test

--- databricks/databricks.tf
+++ databricks/databricks.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_databricks_workspace" "example" {
-  name                = "databricks-test"
+  name                = "databrickstest"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "standard"
