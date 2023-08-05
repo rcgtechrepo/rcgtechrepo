a823ff7eee52489e404a4ff9300416b39f4c8fec
unknown <eric.perler@rcggs.com>
2023-08-05 02:31:47 -0500

    test

--- databricks/main.tf
+++ databricks/main.tf
@@ -1,5 +1,6 @@
 resource "azurerm_resource_group" "example" {
-  name     = "RCG Databricks"
+  name     = "RCG Databrickstls
+  "
   location = "East US"
 }
 
@@ -19,11 +20,16 @@ resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  #name     = random_pet.rg_name.id
-  #name	    = azurerm_resource_group.rg.id
-  #name     = "fedex_rg_00001"
-  name      = var.ENV_NAME
-  
+
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "fedex_nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
 }
--- databricks/outputs.tf
+++ databricks/outputs.tf
@@ -1,12 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "public_ip_address" {
   value = azurerm_databricks_workspace.example.workspace_url
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
 }
