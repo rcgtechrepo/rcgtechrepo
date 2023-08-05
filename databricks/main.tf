512a7970437a12194bf4487f6f478669d55dd4ff
unknown <eric.perler@rcggs.com>
2023-08-05 02:40:09 -0500

    test

--- databricks/main.tf
+++ databricks/main.tf
@@ -36,6 +36,29 @@ resource "azurerm_resource_group" "rg" {
   
 }
 
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "fedex_NetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
 
 resource "azurerm_network_interface" "my_terraform_nic" {
   name                = "fedex_nic"
