937ec06059e7c1e959960f43cee85c6efe113d75
unknown <eric.perler@rcggs.com>
2023-08-10 13:15:21 -0500

    test

--- databricks/main.tf
+++ databricks/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   #name     = random_pet.rg_name.id
   #name	    = azurerm_resource_group.rg.id
-  #name     = "fedex_rg_00001"
+  #name     = "db_rg_00001"
   name      = var.ENV_NAME
   
 }
@@ -35,7 +35,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "fedex_vnet"
+  name                = "db_vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "fedex_subnet"
+  name                 = "db_subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -51,7 +51,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "fedex_PublicIP"
+  name                = "db_PublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
@@ -59,7 +59,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "fedex_NetworkSecurityGroup"
+  name                = "db_NetworkSecurityGroup"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -78,7 +78,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "fedex_nic"
+  name                = "db_nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -123,14 +123,14 @@ resource "tls_private_key" "example_ssh" {
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "fedex_vm"
+  name                  = "db_vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
-    name                 = "fedex_disk"
+    name                 = "db_disk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
